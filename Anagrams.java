7157f64be7fecdf5da8bbc87991ad7be21af8b0b
IrohTwelve <osoleksandr@gmail.com>
2017-07-27 15:27:31 +0300

    now it's two loops

--- Anagrams.java
+++ Anagrams.java
@@ -1,35 +1,32 @@
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 
 public class Anagrams {
+
     public static void main(String[] args) throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-//        simpleStringReverse(reader.readLine());
-        printReverseString(reader.readLine());
+
+        String string = "мама мыла раму";
+        System.out.println(printReverseString(string));
+
     }
 
 
-    public static void printReverseString(String s) {
-        int wordStart = 0;
-        int wordEnd = 0;
-        while (wordStart < s.length() && wordEnd < s.length()) {
-            while (s.charAt(wordEnd) != ' ' && wordEnd < s.length()) {
-                wordEnd++;
-                if (wordEnd >= s.length())
-                    break;
-            }
-            for (int i = wordEnd - 1; i >= wordStart; i--) {
-                System.out.print(s.charAt(i));
-            }
-            wordStart = wordEnd;
-            wordEnd++;
-        }
-    }
+    static String printReverseString(String inputString) {
 
-    public static void simpleStringReverse(String s) {
-        for (int i = s.length() - 1; i >= 0; i--) {
-            System.out.print(s.charAt(i));
+        int stringLength = inputString.length() - 1;
+        String stringToReverse = "";
+        String tempString = "";
+
+        for (int i = 0; i <= stringLength; i++) {
+            tempString = tempString + inputString.charAt(i);
+            if ((inputString.charAt(i) == ' ') || (i == stringLength)) {
+                for (int j = tempString.length() - 1; j >= 0; j--) {
+                    stringToReverse = stringToReverse + tempString.charAt(j);
+                    if ((j == 0) && (i != stringLength))
+                        stringToReverse += " ";
+                }
+                tempString = "";
+            }
         }
+        return stringToReverse;
     }
 }
